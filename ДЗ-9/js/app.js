71fd6a987a1beb0c07678370f3f167b9d3450a63
Gretskiy1989 <ivangretskiy1989@gmail.com>
2021-06-22 17:35:43 +0300

    ДЗ-9 (updated)

--- ДЗ-9/js/app.js
+++ ДЗ-9/js/app.js
@@ -8,56 +8,66 @@ alert( (x > y) ? "x больше, чем y" : "x не больше, чем y" );
 
 let someNumber = prompt('Введите число');
 
-if (someNumber % 2 == 0) {
-  alert( "Число " + someNumber + " четное" );
-} else {
-  alert( "Число " + someNumber + " нечетное" );
-}
+(someNumber % 2 == 0) ? alert( "Число " + someNumber + " четное" ) : alert( "Число " + someNumber + " нечетное" );
+
+ 
+
 
 //task#3
 
-let num = prompt('Введите число', 0);
+let num = +prompt('Введите число');
 
-if (num == 0) {
-  alert( "Число " + num + " нoль" );
-} else if (num > 0 && num < 10) {
-  alert( "Число " + num + " однозначное положительное" );
-} else if (num >= 10 && num < 100) {
-  alert( "Число " + num + " двухзначное положительное" );
-} else if (num >= 100) {
-  alert( "Число " + num + " трехзначное положительное и более" );
-} else if (num < 0 && num > -10) {
-  alert( "Число " + num + " однозначное отрицательное" );
-} else if (num <= -10 && num > -100) {
-  alert( "Число " + num + " двухзначное отрицательное" );
-} else {
-  alert( "Число " + num + " трехзначное отрицательное и более" );
-}
+switch (true) {
+  case num == 0:
+    alert("Число " + num + " нoль");
+    break;
+
+  case num > 0 && num < 10:
+    alert("Число " + num + " однозначное положительное");
+    break;
+
+  case num >= 10 && num < 100:
+    alert("Число " + num + " двухзначное положительное");
+    break;
+
+  case num >= 100:
+    alert("Число " + num + " трехзначное положительное и более");
+    break;
+
+  case num < 0 && num > -10:
+    alert("Число " + num + " однозначное отрицательное");
+    break;
+
+  case num <= -10 && num > -100:
+    alert("Число " + num + " двухзначное отрицательное");
+    break;
+default:
+    alert( "Число " + num + " трехзначное отрицательное и более" );
+} 
+  
 
 //task#4
 
-let a = prompt('Введите число а', '');
-let b = prompt('Введите число b', '');
-let c = prompt('Введите число c', '');
+let a = +prompt('Введите число а');
+let b = +prompt('Введите число b');
+let c = +prompt('Введите число c');
   
-if (a >= b && a >= c) {
-  alert( "Максимальное число " + a );
+switch (true) {
+  case a >= b && a >= c:
+    alert( "Максимальное число " + a );
+    break;
+  case b >= a && b >= c:
+    alert( "Максимальное число " + b );
+  default:
+    alert( "Максимальное число " + c );
 }
-else if (b >= a && b >= c) {
-  alert( "Максимальное число " + b );
-}
-else {
-  alert( "Максимальное число " + c );
-}
+
 
 //task#5
 
-let side1 = prompt('Введите длину стороны "а" предполагаемого треугольника', '');
-let side2 = prompt('Введите длину стороны "b" предполагаемого треугольника', '');
-let side3 = prompt('Введите длину стороны "c" предполагаемого треугольника', '');
+let side1 = +prompt('Введите длину стороны "а" предполагаемого треугольника', '');
+let side2 = +prompt('Введите длину стороны "b" предполагаемого треугольника', '');
+let side3 = +prompt('Введите длину стороны "c" предполагаемого треугольника', '');
+
+let result = (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) ? alert('Треугольник существует!') : alert('При данных условиях треугольник не может существовать!')
  
-if (side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1) {
-alert('Треугольник существует!');
-} else {
-alert('При данных условиях треугольник не может существовать!');
-}
