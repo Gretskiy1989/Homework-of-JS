4722f467600b36b4eb88378e5a9b87abea2e0409
Gretskiy1989 <81696259+Gretskiy1989@users.noreply.github.com>
2021-06-28 19:01:43 +0300

    ДЗ-13 (отредактированно)

--- ДЗ-13/js/app.js
+++ ДЗ-13/js/app.js
@@ -1,19 +1,22 @@
-function loopPow(arg1, arg2) {
-  let res = 1;
-  if(arg1==1 || arg2 == 0) return res;
-  if(arg2==1) return arg1;
-  while (arg2 > 0) {
-      res *= arg1;
-      arg2--;
+function pow(x, n) {
+  let result = 1;
+
+  for (let i = 0; i < n; i++) {
+      result *= x;
   }
-  return res;
+
+  return result;
 }
 
-let arg1  = +prompt('Введите число');
-let arg2 = +prompt('В какую степень Вы хотели бы возвести число введённое выше? Введите число');
+let x  = +prompt('Введите число');
+let n = +prompt('В какую степень Вы хотели бы возвести число введённое выше? Введите число');
 
-if (arg2 < 1) {
-  alert(`Степень ${arg2} не поддерживается, используйте натуральное число`);
-} else {
-  alert('Ваш результат: ' + loopPow(arg1, arg2));
+if (n < 1) {
+  alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} 
+if (typeof n !== "number" || Number.isNaN(n)) {
+  alert('Введённое значение не является числом!');
+}
+else {
+  alert('Ваш результат: ' + pow(x, n));
 }
