e90af3a795cf854d12de830917b35790f55301e1
Gretskiy1989 <ivangretskiy1989@gmail.com>
2021-07-08 22:33:43 +0300

    ДЗ-19

---
+++ ДЗ-19/js/app.js
@@ -0,0 +1,7 @@
+let x = 4;
+let y = 9;
+let r = 10;
+let hupot = Math.sqrt(x*x + y*y);
+
+let result = hupot > r ? "Точка А лежит вне окружности" : "Точка А лежит внутри окружности";
+alert(result);
