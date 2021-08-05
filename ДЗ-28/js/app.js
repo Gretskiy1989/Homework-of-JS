3cda41555957092d942693225917128b7bab757f
Gretskiy1989 <ivangretskiy1989@gmail.com>
2021-08-05 21:57:30 +0300

    ДЗ-28 (исправлено)

--- ДЗ-28/js/app.js
+++ ДЗ-28/js/app.js
@@ -1,15 +1,11 @@
-let person = {
-    name: 'Иван'
-}
+"use strict";
 
-function info(phone, email) {
-    alert(`Имя: ${this.name}, Тел.:${phone}, Email: ${email}`)
-}
+//метод Bind своими руками
 
-//info.bind(person, '+375445363888', 'Gretskiy1989@mail.ru')()
-
-function bind(fn, context) {
-    return fn.bind(context)
-}
-
-bind(info, person)('+375445363888', 'Gretskiy1989@mail.ru')
+const auto = {
+    brand: "Mercedes",
+    drive: function () {
+        return `Вас приветствует компания ${this.brand}`;
+    },
+  };
+  alert(auto.drive()); 
