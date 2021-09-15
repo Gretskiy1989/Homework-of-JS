f9f2ebacb584c796e8114f5b9d1ae22816f775de
Gretskiy1989 <ivangretskiy1989@gmail.com>
2021-09-15 23:06:46 +0300

    Кеширование

--- ДЗ-38 Кеширование (Мемоизация)/js/script.js
+++ ДЗ-38 Кеширование (Мемоизация)/js/script.js
@@ -1,39 +1,44 @@
+"use strict";
+
 // Функция memoizer функция используется для хранения кэша, который индексируется входными параметрами функции. Если нужные аргументы в кэше – кэшированное значение возвращается. В противном случае функция выполняется и вновь вычисленное значение добавляется в кэш.
 
-const memoizer = (func) => {
-  let cache = {}
-  return (n) => {
-      if (cache[n] != undefined) {
-          console.log(`<= Fetching: "${n}" from cache`)
-          return cache[n]
-      } else {
-          console.log(`=> Write: "${n}" in cache`)
-          let result = func(n)
-          cache[n] = result
-          return result
-      }
+const cache = new Map();
+
+function memoizer(user) {
+  
+  if (!cache.has(user)) {
+    console.log(`=> Write: "${user}" in cache`);
+    
+    if (cache.size >= 10) cache.delete(Array.from(cache.keys())[0]);
+      cache.set(user, Date.now());
+    
+    } else {
+      console.log(`<= Fetching: "${user}" from cache`);
   }
+  return cache.get(user);
 }
 
-// функция, которая требует условной оптимизации нагрузки в виде кеширования данных
+  let bentley = { name: "Bentley" };
+  let mercedes_amg = { name: "Mercedes-AMG" };
+  let bugatti = { name: "Bugatti" };
+  let lamborghini = { name: "Lamborghini" };
+  let rolls_royce = { name: "Rolls-Royce" };
+  
+  memoizer(bentley);
+  memoizer(mercedes_amg);
+  memoizer(bugatti);
+  memoizer(lamborghini);
+  memoizer(rolls_royce);
+  memoizer(bentley);
+  memoizer(lamborghini);
+  memoizer(mercedes_amg);
+  memoizer(bugatti);
+  memoizer(rolls_royce);
+  memoizer(bentley);
+  memoizer(mercedes_amg);
+  memoizer(lamborghini);
+  memoizer(bugatti);
+  
+  console.log(cache);
 
-const myFunc = (x) => {
-  return x
-}
 
-const myFuncMem = memoizer(myFunc)
-
-console.log('myFuncMem:', myFuncMem(1000))  // в случае первого вызова функции, данные записываются в "кэш"
-console.log('myFuncMem:', myFuncMem(1000))  // все последующие случаи вызовов берём уже из "кэша"
-console.log('myFuncMem:', myFuncMem(1000))
-console.log('myFuncMem:', myFuncMem(1000))
-console.log('myFuncMem:', myFuncMem(500))  // в случае первого вызова функции, данные записываются в "кэш"
-console.log('myFuncMem:', myFuncMem(500))  // все последующие случаи вызовов берём уже из "кэша"
-console.log('myFuncMem:', myFuncMem(500))
-console.log('myFuncMem:', myFuncMem(500))
-console.log('myFuncMem:', myFuncMem(500))
-console.log('myFuncMem:', myFuncMem('Иван 1989'))  // в случае первого вызова функции, данные записываются в "кэш"
-console.log('myFuncMem:', myFuncMem('Иван 1989'))  // все последующие случаи вызовов берём уже из "кэша"
-console.log('myFuncMem:', myFuncMem('Иван 1989'))
-console.log('myFuncMem:', myFuncMem('Иван 1989'))
-console.log('myFuncMem:', myFuncMem('Иван 1989'))
